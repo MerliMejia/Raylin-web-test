9af7f048f86e06a6f768d6dea6dbd92af0b6c48a
MerliMejia <merlimejia25@gmail.com>
2019-10-01 22:56:11 -0400

    Documentation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,26 @@
 /// <reference path="../../typings/globals/jquery/index.d.ts" />
 
+/**
+ * @author Merli Mejia - merlimejia2@gmail.com
+ * @description This file contains the functionality for the Serving Menu
+ */
+
 var servingMenuItems;
 var selectedServingMenuItems;
 
+/**
+ * @description Init
+ */
 $(window).on("load", (evt)=>{
     servingMenuItems = $(".servingMenu-item");
     selectedServingMenuItems = 0;
     setServingMenuActiveItem(selectedServingMenuItems);
 })
 
+/**
+ * @description This method remove the .servingMenuSelectedItem class from all the non selected items and add it to the selected one
+ * @param {*} index The index number for the selected menu item
+ */
 function setServingMenuActiveItem(index){
     var item = $(servingMenuItems[index]);
     var item2;
